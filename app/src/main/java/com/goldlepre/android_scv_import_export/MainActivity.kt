5b6b3cfd000266275c00035363f154647059b193
adamGT <adamgetachew55@gmail.com>
2023-11-03 18:17:49 +0300

    frst commit

---
+++ app/src/main/java/com/goldlepre/android_scv_import_export/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.goldlepre.android_scv_import_export
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
