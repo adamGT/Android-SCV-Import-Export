751847d3ecead92ef93588bedbad291fc04a9ecd
adamGT <adamgetachew55@gmail.com>
2023-12-27 10:10:18 -0800

    Added File Picker And Implemented CSV file reader

--- app/src/main/java/com/goldlepre/android_scv_import_export/ui/MainActivity.kt
+++ app/src/main/java/com/goldlepre/android_scv_import_export/ui/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.goldlepre.android_scv_import_export.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.goldlepre.android_scv_import_export.adapters.MainListAdapter
 import com.goldlepre.android_scv_import_export.databinding.ActivityMainBinding
+import com.goldlepre.android_scv_import_export.models.UserData
 import com.goldlepre.android_scv_import_export.models.excel.ExcelRepository
+import java.io.InputStream
+
 
 class MainActivity : AppCompatActivity() {
 
+    private val PICKFILE_REQUEST_CODE = 109
+
     private lateinit var binder: ActivityMainBinding
     private lateinit var rep: ExcelRepository
+    private var classTitle = "Empty"
+    private var classInstructor = "Empty"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binder = ActivityMainBinding.inflate(layoutInflater)
@@ -29,9 +40,67 @@ class MainActivity : AppCompatActivity() {
 
 
             importCsvBtn.setOnClickListener {
-
+                openFilePicker()
             }
         }
 
+    }
+
+    @Suppress("DEPRECATION")
+    private fun openFilePicker(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.setType("text/comma-separated-values")
+        startActivityForResult(intent, PICKFILE_REQUEST_CODE)
+    }
+
+    @Deprecated("Deprecated in Java")
+    @Suppress("DEPRECATION")
+    override fun onActivityResult(
+        requestCode: Int, resultCode: Int, resultData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, resultData)
+        if (requestCode == PICKFILE_REQUEST_CODE
+            && resultCode == Activity.RESULT_OK) {
+
+            resultData?.data?.also { uri ->
+                val inputStream: InputStream = contentResolver.openInputStream(uri)!!
+                val data = readCsv(inputStream)
+                if (data.isNotEmpty()) {
+                    Log.d("TAGG", data[0].email)
+
+                    //Do My Logic Here
+
+                } else {
+                    Toast.makeText(applicationContext, "ERROR WITH THE FILE: \nThe File is Empty",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun readCsv(inputStream: InputStream): List<UserData> {
+        var counter = 0;
+        val reader = inputStream.bufferedReader()
+        val header = reader.readLine()
+        Log.d("TAGG",header)
+        if (header.equals("Name,Email,Phone,ClassName,TeacherName")) {
+            return reader.lineSequence()
+                .filter { it.isNotBlank() }
+                .map {
+                    if (counter == 0) {
+                        val (name, email, phone, classTitle, teacher) = it.split(
+                            ',',
+                            ignoreCase = false,
+                            limit = 5
+                        )
+                        this.classTitle = classTitle
+                        this.classInstructor = teacher
+                    }
+                    counter++
+                    val (name, email, phone) = it.split(',', ignoreCase = false, limit = 3)
+                    UserData(name.trim(), email.trim(), phone.trim())
+                }.toList()
+        }else{
+            Toast.makeText(applicationContext, "ERROR WITH THE FILE: \nThe titles(First line) of the CSV file should be [Name,Email,Phone,ClassName,TeacherName]",Toast.LENGTH_LONG).show()
+            return listOf()
+        }
     }
 }
